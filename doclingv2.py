23054eb85ee9eb76ce7661d263597751e3bcca68
Lucas Klemke <lucas.klemke@Outlook.com.br>
2025-04-21 21:58:36 -0300

    Add metadata extraction

--- doclingv2.py
+++ doclingv2.py
@@ -2,8 +2,11 @@ from docling.document_converter import DocumentConverter
 from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.document_converter import DocumentConverter, PdfFormatOption, InputFormat
 from docling.backend.docling_parse_v2_backend import DoclingParseV2DocumentBackend
+import pandas as pd
 
-source = "https://cssjd.org.br/imagens/editor/files/2019/Abril/Tratado%20de%20Fisiologia%20M%C3%A9dica.pdf"
+
+# fonte ( link de pdf )
+source = "http://portal.mec.gov.br/arquivos/pdf/texto.pdf"
 
 pipeline_options = PdfPipelineOptions()
 pipeline_options.do_ocr = False  # pick what you need
@@ -18,6 +21,25 @@ doc_converter = DocumentConverter(
 )
 conv_result = doc_converter.convert(source)
 
-print(conv_result.document.export_to_markdown())
-with open("output.md", "w") as f:
-    f.write(conv_result.document.export_to_markdown())
+print(conv_result)
+print("Document Name:", conv_result.document.origin.filename)
+print("Document Type:", conv_result.document.origin.mimetype)
+print("Number of pages:", len(conv_result.document.pages.keys()))
+
+# Extrair metadata com conteúdo e página
+results_body = conv_result.document.dict()
+dict_list = []
+texts = results_body["texts"]
+for t in texts:
+    ref = t["self_ref"]
+    text_content = t["text"]
+    page = t["prov"][0]["page_no"]
+    dict_list.append({
+        "ref": ref,
+        "text": text_content[:100],
+        "page": page
+    })
+    
+
+df = pd.DataFrame(dict_list)
+print(df)
